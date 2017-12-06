fce31438701694011e346bf68503a2a61baa43d2
Yaodong Zhao <yaodong.zhao@utah.edu>
2017-12-06 13:34:20 -0700

    Setup as a python package

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mappercore',
+    version='0.0.1.dev0',
+    keywords='mapper',
+    description='A framework for data analysis and visualization',
+    license='TBD',
+    url='https://github.com/yaodong/mapper-core',
+    author='Yaodong Zhao',
+    author_email='yaodong.zhao@utah.edu',
+    packages=find_packages(),
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'Flask==0.12.2'
+    ],
+)
