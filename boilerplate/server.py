1182dc75bebe65c50d3633974ac5ad83843131af
Yaodong Zhao <yaodong.zhao@utah.edu>
2018-08-13 16:29:55 -0600

    add boilerplate

---
+++ boilerplate/server.py
@@ -0,0 +1,10 @@
+from mappercore import Server
+
+# create server instance
+server = Server()
+
+# register your backend function here
+
+# start the server
+if __name__ == '__main__':
+    server.run()
