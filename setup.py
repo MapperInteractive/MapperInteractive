a088e460628e872e3c7c423a1dffba39526ab261
Yaodong Zhao <git@yaodong.org>
2019-01-29 21:00:18 -0700

    fix image file url and bump verison

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mappercore',
-    version='1.2.3',
+    version='1.2.4',
     keywords='mapper',
     description='A framework for data analysis and visualization',
     license='MIT',
