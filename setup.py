68c999d570067b0987adb5e12a719008c14909b5
Yaodong Zhao <git@yaodong.org>
2019-01-29 20:53:33 -0700

    rebuild and bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mappercore',
-    version='1.2.1',
+    version='1.2.2',
     keywords='mapper',
     description='A framework for data analysis and visualization',
     license='MIT',
