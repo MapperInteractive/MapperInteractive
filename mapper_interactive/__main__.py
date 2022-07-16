6752673f3eca08d144a159f7eca31321c98ea513
Ethan Rooke <ethan@roo.ke>
2022-07-16 14:18:17 -0500

    Add argument parsing to the main script

--- mapper_interactive/__main__.py
+++ mapper_interactive/__main__.py
@@ -1,4 +1,28 @@
-#!flask/bin/python
+import argparse
+
 from .app import app
 
-app.run(host="127.0.0.1", port=8080, debug=True)
+parser = argparse.ArgumentParser("Run a Mapper Interactive server")
+
+parser.add_argument(
+    "-H",
+    "--host",
+    default="127.0.0.1",
+    help="The host name to serve Mapper Interactive on",
+    type=str,
+)
+parser.add_argument(
+    "-p",
+    "--port",
+    default=5000,
+    help="The port number to serve Mapper Interactive on",
+    type=int,
+)
+parser.add_argument(
+    "-d", "--debug", action="store_true", help="Launch the server in debug mode"
+)
+
+opt = parser.parse_args()
+
+
+app.run(host=opt.host, port=opt.port, debug=opt.debug)
